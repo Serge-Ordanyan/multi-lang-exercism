a9c6d7096ed6e5d1598aad056c9007864e27463e
Ordanyan Serge <ordanyanserge@aol.com>
2026-07-01 22:22:42 +0400

    feat(rust): add full unit tests and verification suite for matching-brackets (#13)

---
+++ rust/matching-brackets/target/debug/deps/matching_brackets-92ac20713712567d.d
@@ -0,0 +1,7 @@
+/Users/serge/Exercism/rust/matching-brackets/target/debug/deps/matching_brackets-92ac20713712567d.d: src/lib.rs
+
+/Users/serge/Exercism/rust/matching-brackets/target/debug/deps/libmatching_brackets-92ac20713712567d.rlib: src/lib.rs
+
+/Users/serge/Exercism/rust/matching-brackets/target/debug/deps/libmatching_brackets-92ac20713712567d.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ rust/matching-brackets/target/debug/deps/matching_brackets-a2a8e379664cc3ef.d
@@ -0,0 +1,5 @@
+/Users/serge/Exercism/rust/matching-brackets/target/debug/deps/matching_brackets-a2a8e379664cc3ef.d: tests/matching-brackets.rs
+
+/Users/serge/Exercism/rust/matching-brackets/target/debug/deps/matching_brackets-a2a8e379664cc3ef: tests/matching-brackets.rs
+
+tests/matching-brackets.rs:
